48a494676ac91641d3271935ae2b1b5bb805cfe8
ZeayW <16307110042@fudan.edu.cn>
2021-09-13 20:07:27 +0800

    Update new_verilog_parser.py

--- util/new_verilog_parser.py
+++ util/new_verilog_parser.py
@@ -871,7 +871,10 @@ class DcParser:
                 intersect = None
                 for j in range(i+1):
                     if j < len(fanin_args[0]):
-                        path = nx.shortest_path(g,fanin_args[0][j],fanout)[1:]
+                        path = nx.shortest_path(g,fanin_args[0][j],fanout)
+                        print(path)
+                        print(path[1:])
+                        exit()
                         intersect = set(path) if intersect is None else set(path) & intersect
                         #print(path)
                 print(i,fanout,intersect)
