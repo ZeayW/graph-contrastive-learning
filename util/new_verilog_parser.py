53f9d2f5fa17f2c9d971dcce5d246ec734ed14a8
ZeayW <16307110042@fudan.edu.cn>
2021-09-13 20:17:15 +0800

    Update new_verilog_parser.py

--- util/new_verilog_parser.py
+++ util/new_verilog_parser.py
@@ -875,7 +875,7 @@ class DcParser:
                         intersect = set(path) if intersect is None else set(path) & intersect
                         print('src:{},dst:{},path:{}'.format(fanin_args[0][j],fanout,path))
                 print(i,fanout,intersect)
-                exit()
+                if i==5:exit()
             for i in in_nodes:
                 fw = dict(nx.bfs_successors(g, i, 6))
                 for t in fw.values():
