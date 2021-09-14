0be77c1f8fbad274f7ad2d7bab94c76eb8ccf6ef
ZeayW <16307110042@fudan.edu.cn>
2021-09-14 19:20:53 +0800

    Update quick_train_dc.py

--- codes/quick_train_dc.py
+++ codes/quick_train_dc.py
@@ -55,7 +55,7 @@ def oversample(g,options,in_dim):
     nodes = th.tensor(range(g.num_nodes()))
     nodes = nodes[no_muldiv_mask]
     labels = labels[no_muldiv_mask]
-    print(labels[labels ==1])
+    print(labels[labels.squeeze(-1) ==1])
     mask_pos = (labels ==1).squeeze(1)
 
     mask_neg = (labels == 0).squeeze(1)
