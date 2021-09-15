c67d20268bb2b0b72eb4164a8fa235aced8ca3d4
ZeayW <16307110042@fudan.edu.cn>
2021-09-15 10:40:57 +0800

    Update FunctionConv.py

--- codes/FunctionConv.py
+++ codes/FunctionConv.py
@@ -157,7 +157,7 @@ class FunctionConv(nn.Module):
     def aggregate_func(self,nodes):
         print(nodes.mailbox['m'].shape,nodes.mailbox['m'])
         gate_types = nodes.data['ntype2']
-        print(gate_types)
+        print(gate_types[gate_types==9])
         res = nodes.data['temp']
         for i in range(get_options().in_dim):
             index= gate_types==i
