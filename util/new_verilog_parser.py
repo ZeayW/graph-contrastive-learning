661cc6a929b31cd818f72eb482bd2b909f112dc3
ZeayW <16307110042@fudan.edu.cn>
2021-09-16 16:52:49 +0800

    Update new_verilog_parser.py

--- util/new_verilog_parser.py
+++ util/new_verilog_parser.py
@@ -742,8 +742,10 @@ class DcParser:
 
                     if ntype == 'IBUFF' or ('DFF' in ntype and fo.portname=='QN'):
                         ntype = 'INV'
-                    
+
                     if buff_replace.get(fo.argname,None) is not None:
+                        if 'DFF' in ntype:
+                            print(ntype,fo.portname)
                         nodes.append((fo.argname, {"type": ntype}))
             #print(len(inputs))
             #print(inputs)
