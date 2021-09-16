2c0263a7170cb4c7a37aad0a5311ce60f3468427
ZeayW <16307110042@fudan.edu.cn>
2021-09-17 01:44:09 +0800

    Update new_verilog_parser.py

--- util/new_verilog_parser.py
+++ util/new_verilog_parser.py
@@ -364,7 +364,7 @@ class DcParser:
                         elif position[0] == 'B' :
                             port_info.is_sub_input2 = True
                         else:
-                            print(port_info.portname)
+                            print(position[0],port_info.portname)
                             assert False
                 else:
                     print(cell_type)
