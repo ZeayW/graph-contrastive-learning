aa7451b806915cbd721afd385fffc0a0664fa86e
ZeayW <16307110042@fudan.edu.cn>
2021-09-16 19:02:20 +0800

    Update quick_train_dc.py

--- codes/quick_train_dc.py
+++ codes/quick_train_dc.py
@@ -494,8 +494,8 @@ def train(options):
     # replaceDFF(train_g)
     # replaceDFF(val_g)
 
-    #train_g.ndata['position'][train_g.ndata['label_o'].squeeze(-1) == -1] = 100
-    #val_g.ndata['position'][val_g.ndata['label_o'].squeeze(-1) == -1] = 100
+    train_g.ndata['position'][train_g.ndata['label_o'].squeeze(-1) == -1] = 100
+    val_g.ndata['position'][val_g.ndata['label_o'].squeeze(-1) == -1] = 100
     #train_g.ndata['position'][train_g.ndata['label_o'].squeeze(-1) == 0] = 100
     #val_g.ndata['position'][val_g.ndata['label_o'].squeeze(-1) == 0] = 100
 
