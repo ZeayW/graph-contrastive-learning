c96f1b0f3103e922dd915c6ce879b07183eeca6b
ZeayW <16307110042@fudan.edu.cn>
2021-09-23 17:28:25 +0800

    .

---
+++ codes/generate_netlist.py
@@ -0,0 +1,16 @@
+
+import os
+from generate_options import get_options
+options = get_options()
+data_dir = os.path.join(options.save_dir,'i{}'.format(options.num_input))
+tcl_file = os.path.join(options.save_dir,'dc.tcl')
+for vf in os.listdir(data_dir):
+    if not vf.endswith('.v') :
+        continue
+    with open(tcl_file,'r') as f:
+        lines = f.readlines()
+    lines[2] = "set numInput \"{}\"".format(options.num_input)
+    lines[3] = "set value \"{}\"".format(vf.split(['.'][0]))
+    with open(tcl_file,'w') as f:
+        f.writelines(lines)
+    os.system('dc_shell-xg-t -f dc.tcl')
--- codes/generate_options.py
+++ codes/generate_options.py
@@ -5,6 +5,7 @@ def get_options(args=None):
     parser = argparse.ArgumentParser()
     parser.add_argument('--num_input',type=int,default=2)
     parser.add_argument('--save_dir',type=str,default='../truthtables')
+    parser.add_argument('--data_dir', type=str, default='../truthtables/i')
     options = parser.parse_args(args)
 
     return options
