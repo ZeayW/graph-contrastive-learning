ac040fd4a2f8f068e6b9aa9b449576337535d4a0
ZeayW <16307110042@fudan.edu.cn>
2021-09-23 19:31:08 +0800

    Create generate_netlist.py

---
+++ truthtables/generate_netlist.py
@@ -0,0 +1,20 @@
+
+import os
+from generate_options import get_options
+os.system('source /opt2/synopsys/setup.sh')
+options = get_options()
+data_dir = os.path.join(options.save_dir,'i{}'.format(options.num_input))
+tcl_file ='dc.tcl'
+for vf in os.listdir(data_dir):
+    if not vf.endswith('.v') :
+        continue
+    with open(tcl_file,'r') as f:
+        lines = f.readlines()
+    lines[2] = "set numInput \"{}\"\n".format(options.num_input)
+    lines[3] = "set value \"{}\"\n".format(vf.split('.')[0])
+    print(lines)
+
+    with open(tcl_file,'w') as f:
+        f.writelines(lines)
+    os.system('dc_shell-xg-t -f dc.tcl')
+    exit()
