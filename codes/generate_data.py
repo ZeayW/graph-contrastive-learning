22fd71eb2a200f9b8492f3257954f436b0cf7ce5
ZeayW <16307110042@fudan.edu.cn>
2021-09-24 20:05:32 +0800

    Update generate_data.py

--- codes/generate_data.py
+++ codes/generate_data.py
@@ -687,19 +687,17 @@ def random_replace(g,nid,id2type,edge2port):
     for j, predecessor in enumerate(predecessors):
         for pi in replace_cell.input_links[j]:
             g.add_edge(predecessor, new_nodes[pi][0])
-    print(sucessors)
     # remove adjacent INVs
     if new_nodes[replace_cell.output_link][1]['ntype'] == 'INV':
-        print(sucessors)
         for sucessor in sucessors:
             if id2type[sucessor] == 'INV':
-                print('\t\tremove:({},{})'.format(new_nodes[replace_cell.output_link][0], sucessor))
+                print('\t\tsuc remove:({},{})'.format(new_nodes[replace_cell.output_link][0], sucessor))
                 remove_adjacent_inv(g, new_nodes[replace_cell.output_link][0], sucessor)
     for j, predecessor in enumerate(predecessors):
         if id2type[predecessor] == 'INV':
             pi = replace_cell.input_links[j][0]
             if new_nodes.get(pi,None) is not None and new_nodes[pi][1]['ntype'] == 'INV':
-                print('\t\tremove:({},{})'.format(predecessor, new_nodes[pi][0]))
+                print('\t\tpre remove:({},{})'.format(predecessor, new_nodes[pi][0]))
                 remove_adjacent_inv(g, predecessor, new_nodes[pi][0])
     return nid
 
