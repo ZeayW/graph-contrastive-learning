08c71407606ff5a98f388a36d431ace2aae0a77d
ZeayW <16307110042@fudan.edu.cn>
2021-09-24 16:39:34 +0800

    Update generate_data.py

--- codes/generate_data.py
+++ codes/generate_data.py
@@ -777,6 +777,7 @@ def  main():
     for vf in os.listdir(datapath):
         if not vf.endswith('.v'):
             continue
+        print('generate positive samples for {}'.format(vf))
         value = vf.split('_')[2][1:]
         parser = DcParser('i{}_v{}'.format(options.num_input,value))
         output_nid,nodes,edges = parser.parse(os.path.join(datapath,vf))
