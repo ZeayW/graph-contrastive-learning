7a5f8c4ca12a8b75ba715e59e4f7d52c94d30bdc
ZeayW <16307110042@fudan.edu.cn>
2021-09-28 19:25:43 +0800

    gcl training

---
+++ codes/dataset_gcl.py
@@ -0,0 +1,152 @@
+import sys
+
+sys.path.append("..")
+import util.structural as structural
+import util.verilog as verilog
+
+import dgl
+from dgl.data import DGLDataset
+import networkx as nx
+import torch as th
+import numpy as np
+import os
+import random
+from util.single_verilog_parser import *
+from generate_options import get_options
+from torch.nn.parameter import Parameter
+from codes.generate_data import *
+
+
+def parse_single_file(nodes,edges,output_node):
+    # nodes: list of (node, {"type": type}) here node is a str ,like 'n123' or '1'b1'
+    # note that here node type does not include buf /not
+    label2id = {"1'b0": 0, "1'b1": 1, 'DFF': 2, 'DFFSSR': 3, 'DFFAS': 4, 'NAND': 5, 'AND': 6,
+                'OR': 7, 'DELLN': 8, 'INV': 9, 'NOR': 10, 'XOR': 11, 'MUX': 12, 'XNOR': 13,
+                'MAJ': 14, 'PI': 15}
+
+    nid = 0
+    node2id = {}
+    id2node = {}
+    output_nid = None
+    ntype = th.zeros((len(node2id), len(label2id.keys())), dtype=th.float)
+    for n in nodes:
+        if node2id.get(n[0]) is None:
+            node2id[n[0]] = nid
+            id2node[nid] = n[0]
+            type = n[1]["type"]
+            if re.search("\d", type):
+                type = type[: re.search("\d", type).start()]
+            ntype[nid][label2id[type]] = 1
+            if n[0] == output_node:
+                output_nid = nid
+            nid += 1
+
+    src_nodes = []
+    dst_nodes = []
+
+    for src, dst, edict in edges:
+        src_nodes.append(node2id[src])
+        dst_nodes.append(node2id[dst])
+
+    graph = dgl.graph(
+        (th.tensor(src_nodes), th.tensor(dst_nodes)), num_nodes=len(node2id)
+    )
+
+    graph.ndata["ntype"] = ntype
+    PIs = th.tensor(range(graph.number_of_nodes()))[ntype==15]
+    print(len(PIs))
+    depth = cal_depth(graph,PIs,output_nid)
+
+    return graph,output_nid,depth
+
+    
+class Dataset_gcl(DGLDataset):
+    def __init__(self,width,datapath):
+        self.datapath = datapath
+        super(Dataset_gcl, self).__init__(name="dac")
+        # self.alpha = Parameter(th.tensor([1]))
+
+    def process(self):
+        type2label = {"ling_adder":1,"hybrid_adder":2, "cond_sum_adder":3, "sklansky_adder":4, "brent_kung_adder":5, "bounded_fanout_adder":6,"unknown":7}
+        self.batch_graphs = []
+        self.graphs = []
+        self.len = 0
+        max_depth = 0
+        options = get_options()
+        start_nid = 0
+        self.POs = {}
+        for vf in os.listdir(self.datapath):
+            if not vf.endswith('.v'):
+                continue
+            PO = []
+            print('\ngenerate positive samples for {}'.format(vf))
+            value = vf.split('_')[2][1:]
+            parser = DcParser('i{}_v{}'.format(options.num_input, value))
+            output_node, nodes, edges = parser.parse(os.path.join(self.datapath, vf))
+            if len(nodes) == 0:
+                print('empty...')
+                continue
+            original_graph, original_PO, original_depth = parse_single_file(nodes, edges, output_node)
+            self.POs[original_PO+start_nid] = original_depth
+            PO.append((original_PO+start_nid,original_depth))
+            self.graphs.append(original_graph)
+            start_nid += original_graph.number_of_nodes()
+            if original_depth>max_depth:
+                max_depth = original_depth
+            for i in range(2):
+                new_nodes, new_edges = transform(nodes, edges, options)
+                new_graph, new_PO, new_depth = parse_single_file(new_nodes, new_edges, output_node)
+                self.POs[new_PO+start_nid] = new_depth
+                self.graphs.append(new_graph)
+                start_nid += new_graph.number_of_nodes()
+                if new_depth>max_depth:
+                    max_depth = new_depth
+        self.depth = max_depth
+        self.batch_graph = dgl.batch(self.graphs)
+
+
+    def __len__(self):
+        return self.len
+
+
+def change_order(nids,width):
+    res = []
+    if len(nids) % width !=0 :
+        print('num of IO is wrong')
+        assert False
+    num_modules = int(len(nids) / width)
+    for bit_index in range(width):
+        for module_index in range(num_modules):
+            res.append(nids[width*module_index+bit_index])
+    return res
+def cal_depth(g,PIs,output_nid):
+    g = g.to_networkx()
+    depth = 0
+
+    dst = output_nid
+    for src in PIs:
+        #print('src',id2nodes[src])
+        max_path_length = 0
+        #for dst in PIs[2*i:2*i+2]:
+        path = None
+        if nx.has_path(g, src, dst):
+            for p in nx.all_simple_paths(g, src, dst):
+                if len(p) > max_path_length:
+                    max_path_length = len(p)
+                    path = p
+
+        depth = max(depth,max_path_length)
+
+    return depth
+
+
+if __name__ == "__main__":
+    random.seed(726)
+    datapaths = ["../dc/sub/implementation/test/"]
+
+    th.multiprocessing.set_sharing_strategy('file_system')
+    # print(dataset_not_edge.Dataset_n)
+    dataset = Dataset_sub("adder", 32,datapaths, None)
+
+    # graph = parse_single_file('../util1/adder4_d0.00_auto_adder.v')
+    # print(graph)
--- codes/options.py
+++ codes/options.py
@@ -72,6 +72,7 @@ def get_options(args=None):
     parser.add_argument('--sub',action='store_true')
     parser.add_argument('--muldiv',action='store_true')
     parser.add_argument('--add', type=int,default=0)
+    parser.add_argument('--num_input', type=int, default=3)
     #parser.add_argument('--in')
     options = parser.parse_args(args)
 
