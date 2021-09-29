732262ac4536b39fa3539f499656fba237713137
ZeayW <16307110042@fudan.edu.cn>
2021-09-29 22:35:46 +0800

    Update generate_options.py

--- truthtables/generate_options.py
+++ truthtables/generate_options.py
@@ -1,7 +1,7 @@
 import argparse
 
 
-def get_options_g(args=None):
+def get_options(args=None):
     parser = argparse.ArgumentParser()
     parser.add_argument('--num_input',type=int,default=2)
     parser.add_argument('--save_dir',type=str,default='../truthtables')
