d25f0e14596c7ef225d5eac09499b63c7b713ce6
ZeayW <16307110042@fudan.edu.cn>
2021-10-05 20:35:06 +0800

    Update generate_truthtable.py

--- truthtables/generate_truthtable.py
+++ truthtables/generate_truthtable.py
@@ -182,3 +182,21 @@ while current_num<num_sample:
         f.write('endmodule\n')
 
 print('num visited:',len(visited),'total',pow(2,pow(2,num_input)))
+
+os.system('source /opt2/synopsys/setup.sh')
+options = get_options()
+data_dir = os.path.join(options.save_dir,'i{}'.format(options.num_input))
+tcl_file = os.path.join(options.save_dir,'dc.tcl')
+for vf in os.listdir(data_dir):
+    if not vf.endswith('.v') :
+        continue
+    with open(tcl_file,'r') as f:
+        lines = f.readlines()
+    lines[2] = "set numInput \"{}\"\n".format(options.num_input)
+    lines[3] = "set value \"{}\"\n".format(vf.split('.')[0])
+    print(lines)
+
+    with open(tcl_file,'w') as f:
+        f.writelines(lines)
+    os.system('dc_shell-xg-t -f {}'.format(tcl_file))
+    exit()
