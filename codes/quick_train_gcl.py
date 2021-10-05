f2fb86c053526b4b5f374966c4b010b081a118ec
ZeayW <16307110042@fudan.edu.cn>
2021-10-05 14:13:06 +0800

    Update quick_train_gcl.py

--- codes/quick_train_gcl.py
+++ codes/quick_train_gcl.py
@@ -246,8 +246,10 @@ def train(options):
             original_nids.append(PO_nids[i])
             aug_nids.append(PO_nids[i+1])
             aug_nids.append(PO_nids[i+2])
-        print(PO_nids)
-        print(len(POs))
+        for po in PO_nids:
+            print(len(train_g.predecessors(po)))
+        #print(PO_nids)
+        #print(len(POs))
         print('num samples',len(aug_nids))
         data_size = len(aug_nids)
         if data_size>options.batch_size:
