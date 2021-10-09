45a8fd3355af654bb3a4c7be7e2abd071d8c1378
ZeayW <16307110042@fudan.edu.cn>
2021-10-09 14:40:34 +0800

    Create merge_data.py

---
+++ codes/merge_data.py
@@ -0,0 +1,28 @@
+import pickle
+import os
+import sys
+import  dgl
+num_input = sys.argv[1]
+num_split = sys.argv[2]
+
+datapath = '../data/gcl_new/i{}/'.format(num_input)
+
+graphs = []
+res_POs = {}
+start_nid = 0
+for i in range(num_split):
+    with open(os.path.join(datapath,'split{}.pkl'.format(i+1)), 'rb') as f:
+        graph, POs, depth = pickle.load(f)
+    start_nid += graph.number_of_nodes()
+    for node,dp in POs.items():
+        res_POs[node+start_nid] = dp
+    graphs.append(graph)
+    print('split{}, num_nodes:{}, num_pos:{}'.format(num_input,graph.number_of_nodes(),len(POs)))
+
+batch_graph = dgl.batch(graphs)
+
+with open(os.path.join(datapath,'merge.pkl'),'wb') as f:
+    pickle.dump((batch_graph,res_POs,depth),f)
+
+
+
