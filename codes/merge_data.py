54d3fe565966f6d7f0f6feb6b93374d51119f123
ZeayW <16307110042@fudan.edu.cn>
2021-10-11 11:05:17 +0800

    Update merge_data.py

--- codes/merge_data.py
+++ codes/merge_data.py
@@ -14,26 +14,28 @@ orignal_graphs,aug_graphs = [],([],[],[])
 original_depth = 0
 depths = [0,0,0]
 start_nid = 0
+pre_nid = 0
 for k in range(int(num_split)):
+
     with open(os.path.join(datapath,'{}.pkl'.format(k+1)), 'rb') as f:
         graph, POs, depth = pickle.load(f)
     graphs = dgl.unbatch(graph)
     PO_nids = list(POs.keys())
     for i in range(0, len(POs), 7):
-        original_nids.append(PO_nids[i]-start_nid)
+        original_nids.append(PO_nids[i]-start_nid+pre_nid)
         start_nid += graphs[i].number_of_nodes()
         original_depth = max(original_depth, POs[PO_nids[i]])
         orignal_graphs.append(graphs[i])
         for j in range(3):
             depths[j] = max(depths[j], POs[PO_nids[i + 1 + 2 * j]])
             depths[j] = max(depths[j], POs[PO_nids[i + 2 + 2 * j]])
-            aug_nids[j].append(PO_nids[i + 1 + 2 * j]-start_nid)
-            aug_nids[j].append(PO_nids[i + 2 + 2 * j]-start_nid )
+            aug_nids[j].append(PO_nids[i + 1 + 2 * j]-start_nid+pre_nid)
+            aug_nids[j].append(PO_nids[i + 2 + 2 * j]-start_nid+pre_nid )
             start_nid += graphs[i + 1 + 2 * j].number_of_nodes()
             start_nid += graphs[i + 2 + 2 * j].number_of_nodes()
             aug_graphs[j].append(graphs[i + 1 + 2 * j])
             aug_graphs[j].append(graphs[i + 2 + 2 * j])
-
+    pre_nid += graph.number_of_nodes()
 
 
     print('split{}, num_nodes:{}, num_pos:{}'.format(k,graph.number_of_nodes(),len(POs)))
