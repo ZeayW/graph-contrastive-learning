8c556b4a2caf80e868ec301cba11c3a00a1789d3
ZeayW <16307110042@fudan.edu.cn>
2021-10-13 19:42:38 +0800

    Update quick_train_ft.py

--- codes/quick_train_ft.py
+++ codes/quick_train_ft.py
@@ -521,18 +521,18 @@ def train(options):
     print(options.nlabels)
     print(Loss)
     optim = th.optim.Adam(
-        itertools.chain(mlp.parameters(),
-                        model.conv.gate_functions[2].parameters(),
-                        model.conv.gate_functions[3].parameters(),
-                        model.conv.gate_functions[4].parameters(),
-                        model.conv.gate_functions[8].parameters()),
-        #mlp.parameters(),
+        # itertools.chain(mlp.parameters(),
+        #                 model.conv.gate_functions[2].parameters(),
+        #                 model.conv.gate_functions[3].parameters(),
+        #                 model.conv.gate_functions[4].parameters(),
+        #                 model.conv.gate_functions[8].parameters()),
+        mlp.parameters(),
         options.learning_rate, weight_decay=options.weight_decay
     )
 
-    for i in range(options.in_dim):
-        if i in (2,3,4,8):
-            model.conv.gate_functions[i].train()
+    # for i in range(options.in_dim):
+    #     if i in (2,3,4,8):
+    #         model.conv.gate_functions[i].train()
     #model.train()
     mlp.train()
     # if model.GCN1 is not None:model.GCN1.train()
