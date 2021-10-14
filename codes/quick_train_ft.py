7b497a678ec56d0bd900eb68f048f1d2081be5fe
ZeayW <16307110042@fudan.edu.cn>
2021-10-14 22:33:10 +0800

    Update quick_train_ft.py

--- codes/quick_train_ft.py
+++ codes/quick_train_ft.py
@@ -579,15 +579,15 @@ def train(options):
         #                 model.conv.gate_functions[3].parameters(),
         #                 model.conv.gate_functions[4].parameters(),
         #                 model.conv.gate_functions[8].parameters()),
-        #itertools.chain(mlp.parameters(),model.parameters()),
-        mlp.parameters(),
+        itertools.chain(mlp.parameters(),model.parameters()),
+        #mlp.parameters(),
         options.learning_rate, weight_decay=options.weight_decay
     )
 
     # for i in range(options.in_dim):
     #     if i in (2,3,4,8):
     #         model.conv.gate_functions[i].train()
-    #model.train()
+    model.train()
     mlp.train()
     # if model.GCN1 is not None:model.GCN1.train()
     # if model.GCN2 is not None:model.GCN2.train()
