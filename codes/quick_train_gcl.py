ee317f1f9bd77d807e53cb81a64e446294e82260
ZeayW <16307110042@fudan.edu.cn>
2021-10-21 23:11:18 +0800

    Update quick_train_gcl.py

--- codes/quick_train_gcl.py
+++ codes/quick_train_gcl.py
@@ -232,8 +232,6 @@ def train(options):
     data_loaders = []
     for num_input in range(start_input,options.num_input+1):
         print('num_input{}'.format(num_input))
-        if num_input!=5:
-            break
         origin_file = os.path.join(data_path,'i{}/new_origin.pkl'.format(num_input))
         aug_files = [os.path.join(data_path,'i{}/new_aug{}.pkl'.format(num_input,i)) for i in range(start_aug,4)]
         for i ,file in enumerate(aug_files):
