fae307e525e99dc0b8123fd0a856f2e7a5aa43a3
ZeayW <16307110042@fudan.edu.cn>
2021-11-03 13:47:15 +0800

    Update quick_train_ft.py

--- codes/quick_train_ft.py
+++ codes/quick_train_ft.py
@@ -584,7 +584,7 @@ def train(options):
     val_g.ndata['ntype2'] = th.argmax(val_g.ndata['ntype'], dim=1).squeeze(-1)
     val_graphs = dgl.unbatch(val_g)
 
-    print('num 1b0', len(val_g.ndata['ntype2']==0),'num 1b0', len(val_g.ndata['ntype2']==1))
+    print('num 1b0', len(val_g.ndata['ntype2'][val_g.ndata['ntype2']==0]),'num 1b0', len(val_g.ndata['ntype2'][val_g.ndata['ntype2']==1]))
 
     if options.add == 2:
         #train_g.ndata['label_o'][train_g.ndata['adder_o'].squeeze(-1) == 1] = -1
