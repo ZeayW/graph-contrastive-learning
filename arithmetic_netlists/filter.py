7bd7d74d5fdce154b3bba72675b1e9ea5073a3d5
ZeayW <16307110042@fudan.edu.cn>
2021-11-04 20:21:16 +0800

    Update filter.py

--- arithmetic_netlists/filter.py
+++ arithmetic_netlists/filter.py
@@ -3,7 +3,7 @@ import os
 dir = './adders/'
 for folder in os.listdir(dir):
     if os.path.isdir(folder):
-
+        print(folder)
         for subfolder in os.listdir(dir+folder):
             if subfolder == 'implementation':
                 print(dir+folder+'/implementation')
