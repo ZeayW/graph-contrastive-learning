4d20ec3d3706bf105d3e29e983f80780f0d31a94
ZeayW <16307110042@fudan.edu.cn>
2021-11-04 20:24:29 +0800

    Update filter.py

--- arithmetic_netlists/filter.py
+++ arithmetic_netlists/filter.py
@@ -6,8 +6,13 @@ for folder in os.listdir(dir):
         for subfolder in os.listdir(dir+folder):
             if subfolder == 'implementation':
                 print(dir+folder+'/implementation')
+                size_dict = {}
                 for vf in os.listdir(dir+folder+'/implementation'):
-                    if not vf.endswith('.v'):
+                    if not vf.endswith('.v') or 'hier' in vf:
                         continue
                     print(os.path.getsize(dir+folder+'/implementation/'+vf))
+                    size = os.path.getsize(dir+folder+'/implementation/'+vf)
+                    size_dict[size] = size_dict.get(size,[])
+                    size_dict[size].append(dir+folder+'/implementation/'+vf)
+                print(size_dict)
         exit()
