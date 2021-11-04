0d8c3df7db44fdb1a78bdb789a0c65daccffa0b9
ZeayW <16307110042@fudan.edu.cn>
2021-11-04 20:20:18 +0800

    Create filter.py

---
+++ arithmetic_netlists/filter.py
@@ -0,0 +1,14 @@
+import os
+
+dir = './adders/'
+for folder in os.listdir(dir):
+    if os.path.isdir(folder):
+
+        for subfolder in os.listdir(dir+folder):
+            if subfolder == 'implementation':
+                print(dir+folder+'/implementation')
+                for vf in os.listdir(dir+folder+'/implementation'):
+                    if not vf.endswith('.v'):
+                        continue
+                    print(os.path.getsize(dir+folder+'/implementation/'+vf))
+        exit()
