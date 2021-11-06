c64cf7be083046cdb30c2419eef3dc2dc99580f1
ZeayW <16307110042@fudan.edu.cn>
2021-11-06 18:06:27 +0800

    Update quick_train_global.py

--- codes/quick_train_global.py
+++ codes/quick_train_global.py
@@ -470,7 +470,7 @@ def train(options):
         labels = th.tensor([]).to(device)
         global_embeddings = th.tensor([]).to(device)
         for idx,(label,graph,POs,depth) in enumerate(train_graphs):
-            th.cat((labels,th.tensor([label].to(device))))
+            th.cat((labels,th.tensor([label]).to(device)))
             sampler = Sampler([None] * depth, include_dst_in_src=options.include)
             dataloader = MyNodeDataLoader(
                 True,
