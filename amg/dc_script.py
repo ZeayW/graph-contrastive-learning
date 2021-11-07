0b1c7a4974d4379dd17a466909fe98e208a13375
ZeayW <16307110042@fudan.edu.cn>
2021-11-07 21:02:45 +0800

    Update dc_script.py

--- amg/dc_script.py
+++ amg/dc_script.py
@@ -9,13 +9,13 @@ for folder in os.listdir(dir):
     l = lines[3]
     #print(l)
     top_module = l.split(':')[1].replace(' ','').replace('\n','')
-    for i in range(1,7):
-        with open('dc{}.tcl'.format(i),'r') as f:
-            lines = f.readlines()
-        lines[0] = 'set top_module "{}"\n'.format(top_module)
-        lines[1] = 'set opt "{}"\n'.format(folder)
-        lines[2] = 'set src "{}"\n'.format(design_file)
-        with open('dc{}_{}.tcl'.format(i, folder), 'w') as f:
-            f.writelines(lines)
-        os.system('dc_shell-xg-t -f dc{}_{}.tcl'.format(i, folder))
-        os.remove('dc{}_{}.tcl'.format(i, folder))
+
+    with open('dc.tcl', 'r') as f:
+        lines = f.readlines()
+    lines[0] = 'set top_module "{}"\n'.format(top_module)
+    lines[1] = 'set opt "{}"\n'.format(folder)
+    lines[2] = 'set src "{}"\n'.format(design_file)
+    with open('dc_{}.tcl'.format(folder), 'w') as f:
+        f.writelines(lines)
+    os.system('dc_shell-xg-t -f dc{}_{}.tcl'.format(folder))
+    os.remove('dc{}_{}.tcl'.format(folder))
