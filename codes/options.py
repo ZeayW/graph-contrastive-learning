34dff4b858ed7cc9bb0337d1d4255d07d83f9afd
ZeayW <16307110042@fudan.edu.cn>
2021-11-09 00:48:19 +0800

    Update options.py

--- codes/options.py
+++ codes/options.py
@@ -78,7 +78,7 @@ def get_options(args=None):
     parser.add_argument('--split', type=int, default=1)
     parser.add_argument('--start',type=int, nargs='+', default=[5,1])
     parser.add_argument('--end', type=int, nargs='+', default=[7, 3])
-    parser.add_argument('--train_percent',type=int,default=6)
+    parser.add_argument('--train_percent',type=float,default=6)
     parser.add_argument('--loss_thred',type=float,default=0.1)
     parser.add_argument('--freeze',action='store_true')
     #parser.add_argument('--in')
