0a0fa9c759e9e545433df3da94cf95501e887775
ZeayW <16307110042@fudan.edu.cn>
2021-11-09 21:17:00 +0800

    .

--- amg/filter.py
+++ amg/filter.py
@@ -3,22 +3,17 @@ import sys
 target = sys.argv[1]
 dir = './{}/'.format(target)
 
-for folder in os.listdir(dir):
-    if os.path.isdir(dir+folder):
-        for subfolder in os.listdir(dir+folder):
-            if subfolder == 'implementation':
-                #print(dir+folder+'/implementation')
-                size_dict = {}
-                for vf in os.listdir(dir+folder+'/implementation'):
-                    if not vf.endswith('.v') or 'hier' in vf:
-                        continue
-                    #print(os.path.getsize(dir+folder+'/implementation/'+vf))
-                    size = os.path.getsize(dir+folder+'/implementation/'+vf)
-                    size_dict[size] = size_dict.get(size,[])
-                    size_dict[size].append(dir+folder+'/implementation/'+vf)
-                #print(size_dict)
-                for s in size_dict.keys():
-                    for i, path in enumerate(size_dict[s]):
-                        if i!=0:
-                            os.remove(path)
-                print(dir+folder+'/implementation',len(size_dict))
+size_dict = {}
+for vf in os.listdir(dir +  '/implementation'):
+    if not vf.endswith('.v') or 'hier' in vf:
+        continue
+    # print(os.path.getsize(dir+folder+'/implementation/'+vf))
+    size = os.path.getsize(dir +  '/implementation/' + vf)
+    size_dict[size] = size_dict.get(size, [])
+    size_dict[size].append(dir +  '/implementation/' + vf)
+# print(size_dict)
+for s in size_dict.keys():
+    for i, path in enumerate(size_dict[s]):
+        if i != 0:
+            os.remove(path)
+print(dir +  '/implementation', len(size_dict))
