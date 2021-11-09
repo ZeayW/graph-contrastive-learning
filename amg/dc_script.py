1eedac179010bd830ca9da962e593161c9569c25
ZeayW <16307110042@fudan.edu.cn>
2021-11-09 14:55:37 +0800

    Update dc_script.py

--- amg/dc_script.py
+++ amg/dc_script.py
@@ -1,10 +1,11 @@
 import os
 dir = 'multiplier'
 
-for folder in os.listdir(dir):
-    sub_dir = dir + '/'+ folder
-    design_file = os.path.join(sub_dir,'multilier_dc.v')
-    with open(design_file,'r') as f:
+for file in os.listdir(dir):
+    #sub_dir = dir + '/'+ folder
+    #design_file = os.path.join(sub_dir,'multilier_dc.v')
+    target = file.split('.')[0]
+    with open(file,'r') as f:
         lines = f.readlines()
     l = lines[3]
     #print(l)
@@ -13,9 +14,9 @@ for folder in os.listdir(dir):
     with open('dc.tcl', 'r') as f:
         lines = f.readlines()
     lines[0] = 'set top_module "{}"\n'.format(top_module)
-    lines[1] = 'set opt "{}"\n'.format(folder)
-    lines[2] = 'set src "{}"\n'.format(design_file)
-    with open('dc_{}.tcl'.format(folder), 'w') as f:
+    lines[1] = 'set opt "{}"\n'.format(target)
+    lines[2] = 'set src "{}"\n'.format(os.path.join(dir,file))
+    with open('dc_{}.tcl'.format(target), 'w') as f:
         f.writelines(lines)
-    os.system('dc_shell-xg-t -f dc_{}.tcl'.format(folder))
-    os.remove('dc_{}.tcl'.format(folder))
+    os.system('dc_shell-xg-t -f dc_{}.tcl'.format(target))
+    os.remove('dc_{}.tcl'.format(target))
