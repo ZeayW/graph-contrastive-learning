bd3dab0930fc61d090782585833146f17dd24f7e
ZeayW <16307110042@fudan.edu.cn>
2021-11-10 22:17:42 +0800

    Update filter.py

--- arithmetic_netlists/filter.py
+++ arithmetic_netlists/filter.py
@@ -3,22 +3,23 @@ import sys
 dir = sys.argv[1]
 
 for folder in os.listdir(dir):
-    if os.path.isdir(dir+folder):
-        for subfolder in os.listdir(dir+folder):
+    #print(folder)
+    if os.path.isdir(os.path.join(dir,folder)):
+        for subfolder in os.listdir(os.path.join(dir,folder)):
             if subfolder == 'implementation':
-                print("aaa")
+                #print("aaa")
                 #print(dir+folder+'/implementation')
                 size_dict = {}
-                for vf in os.listdir(dir+folder+'/implementation'):
+                for vf in os.listdir(os.path.join(dir,folder+'/implementation')):
                     if not vf.endswith('.v') or 'hier' in vf:
                         continue
                     #print(os.path.getsize(dir+folder+'/implementation/'+vf))
-                    size = os.path.getsize(dir+folder+'/implementation/'+vf)
+                    size = os.path.getsize(os.path.join(dir,folder+'/implementation/'+vf))
                     size_dict[size] = size_dict.get(size,[])
-                    size_dict[size].append(dir+folder+'/implementation/'+vf)
+                    size_dict[size].append(os.path.join(dir,folder+'/implementation/'+vf))
                 #print(size_dict)
                 for s in size_dict.keys():
                     for i, path in enumerate(size_dict[s]):
                         if i!=0:
                             os.remove(path)
-                print(dir+folder+'/implementation',len(size_dict))
+                print(os.path.join(dir,folder+'/implementation'),len(size_dict))
