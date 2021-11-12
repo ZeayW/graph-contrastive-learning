0155629d8d84f4ce472e795af83c41c7f1955166
ZeayW <16307110042@fudan.edu.cn>
2021-11-12 20:39:55 +0800

    Update quick_train_global2.py

--- codes/quick_train_global2.py
+++ codes/quick_train_global2.py
@@ -364,7 +364,7 @@ def train(options):
 
     graphs = []
     #targets = ['adder','multiplier','divider','accumulator','subtractor']
-    targets = ['adder','multiplier','divider','subtractor']
+    targets = ['adder','multiplier','divider','accumulator','subtractor']
     val_graphs = []
     train_graphs = []
 
