1b888f3c0a212fcfcde65ac32c5d855cff49de1f
ZeayW <16307110042@fudan.edu.cn>
2021-11-18 14:44:42 +0800

    Update quick_train_global3.py

--- codes/quick_train_global3.py
+++ codes/quick_train_global3.py
@@ -179,8 +179,8 @@ def validate(val_graphs,device,model,mlp,combine,Loss,alpha,beta,options):
         predict_labels = th.argmax(nn.functional.softmax(label_hats, 1), dim=1)
         error_mask = predict_labels != labels
 
-        print('val ground-truth labels:', labels[error_mask])
-        print('val predict labels:', predict_labels[error_mask])
+        #print('val ground-truth labels:', labels[error_mask])
+        #print('val predict labels:', predict_labels[error_mask])
         val_loss = Loss(label_hats, labels)
         #total_num += len(labels)
         #total_loss += train_loss.item() * len(labels)
