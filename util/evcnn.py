3d485be635c0dff8a8ae00660a34036ed8dbea9e
ZeayW <16307110042@fudan.edu.cn>
2021-11-19 21:25:05 +0800

    Update evcnn.py

--- util/evcnn.py
+++ util/evcnn.py
@@ -189,6 +189,7 @@ def train():
             labels = th.cat((labels, th.tensor([label], dtype=th.long).to(device)))
             feature = feature.to(device)
             label_hat = model(feature)
+            print(label_hat.shape)
             print(label,label_hat)
             if label_hats is None:
                 label_hats = label_hat.unsqueeze(0)
