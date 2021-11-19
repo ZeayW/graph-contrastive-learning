ada3206b535149909391e89de7b022ec7cc85afa
ZeayW <16307110042@fudan.edu.cn>
2021-11-19 12:20:18 +0800

    Update t-SNE.py

--- util/t-SNE.py
+++ util/t-SNE.py
@@ -1,51 +1,9 @@
 from yellowbrick.text import TSNEVisualizer
 import yellowbrick
 from yellowbrick import download
-from sklearn.feature_extraction.text import TfidfVectorizer
-import os
-from sklearn.datasets.base import Bunch
+
 import torch as th
 import pickle
-def load_corpus(path):
-    """
-    Loads and wrangles the passed in text corpus by path.
-    """
-
-    # Check if the data exists, otherwise download or raise
-    if not os.path.exists(path):
-        yellowbrick.download.download_all('data')
-        raise ValueError((
-            "'{}' dataset has not been downloaded, "
-            "use the yellowbrick.download module to fetch datasets"
-        ).format(path))
-
-    # Read the directories in the directory as the categories.
-    categories = [
-        cat for cat in os.listdir(path)
-        if os.path.isdir(os.path.join(path, cat))
-    ]
-
-    files  = [] # holds the file names relative to the root
-    data   = [] # holds the text read from the file
-    target = [] # holds the string of the category
-
-    # Load the data from the files in the corpus
-    for cat in categories:
-        for name in os.listdir(os.path.join(path, cat)):
-            files.append(os.path.join(path, cat, name))
-            target.append(cat)
-
-            with open(os.path.join(path, cat, name), 'r',encoding='utf-8') as f:
-                data.append(f.read())
-
-
-    # Return the data bunch for use similar to the newsgroups example
-    return Bunch(
-        categories=categories,
-        files=files,
-        data=data,
-        target=target,
-    )
 
 
 device = th.device("cuda" if th.cuda.is_available() else "cpu")
