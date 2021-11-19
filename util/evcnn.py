9e991e1dfc88a30795fcda93412965db0f70e7fe
ZeayW <16307110042@fudan.edu.cn>
2021-11-19 21:48:44 +0800

    Update evcnn.py

--- util/evcnn.py
+++ util/evcnn.py
@@ -176,12 +176,12 @@ def train():
 
     if os.path.exists(options.model_saving_dir):
         with open(os.path.join(options.model_saving_dir,'model.pkl'),'rb') as f:
-            model = pickle.load(f)
+            options,model = pickle.load(f)
     else:
         os.makedirs(options.model_saving_dir, exist_ok=True)
         model =EVCNN(16)
         with open(os.path.join(options.model_saving_dir,'model.pkl'),'wb') as f:
-            pickle.dump(model,f)
+            pickle.dump((options,model),f)
         with open(os.path.join(options.model_saving_dir, 'res.txt'),'wb') as f:
             pass
     model.to(device)
